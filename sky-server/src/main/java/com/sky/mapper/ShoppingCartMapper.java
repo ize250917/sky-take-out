0460bbdb8e1939a63b595878f9d3f5c837519999
ize250917 <GitHub>
2026-09-08 21:28:38 +0800

    添加购物车代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/ShoppingCartMapper.java
@@ -0,0 +1,34 @@
+package com.sky.mapper;
+
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.ShoppingCart;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface ShoppingCartMapper {
+
+    /*
+    * 动态条件查询购物车
+    */
+    List<ShoppingCart> list(ShoppingCart shoppingCart);
+
+    /**
+     * 根据id修改商品数量
+     */
+    @Update("update shopping_cart set number = #{number} where id = #{id}")
+    void updateNumberById(ShoppingCart shoppingCart);
+
+    /**
+     * 添加购物车
+     * @param shoppingCart
+     */
+    @Insert("insert into shopping_cart " +
+            "(name, user_id, dish_id, setmeal_id,dish_flavor, image, amount, number, create_time) " +
+            "values " +
+            "(#{name},#{userId}, #{dishId}, #{setmealId}, #{dishFlavor}, #{image}, #{amount}, #{number}, #{createTime})")
+    void insert(ShoppingCart shoppingCart);
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/ShoppingCartServiceImpl.java
@@ -0,0 +1,79 @@
+package com.sky.service.impl;
+
+import com.sky.context.BaseContext;
+import com.sky.dto.ShoppingCartDTO;
+import com.sky.entity.Dish;
+import com.sky.entity.Setmeal;
+import com.sky.entity.ShoppingCart;
+import com.sky.mapper.DishMapper;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.ShoppingCartMapper;
+import com.sky.service.ShoppingCartService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@Slf4j
+public class ShoppingCartServiceImpl implements ShoppingCartService {
+
+    @Autowired
+    private ShoppingCartMapper shoppingCartMapper;
+    @Autowired
+    private DishMapper dishMapper;
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    /*
+    * 添加购物车
+    */
+    public void addShoppingCart(ShoppingCartDTO shoppingCartDTO) {
+        //思路：添加购物车时，需要先去判断购物车中是否有该商品
+        //如果有，更新商品的数量即可
+        //如果没有，再去添加商品到购物车
+
+        //查询套餐思路： select * from shopping_cart where user_id = ? and setmeal_id = ?
+        //查询菜品思路： select * from shopping_cart where user_id = ? and dish_id = ? and dish_flavor = ?
+        //直接使用动态条件查询购物车，判断是否有该商品
+        ShoppingCart shoppingCart = new ShoppingCart();
+        BeanUtils.copyProperties(shoppingCartDTO, shoppingCart);
+        Long userId = BaseContext.getCurrentId();
+        shoppingCart.setUserId(userId);
+        List<ShoppingCart> list = shoppingCartMapper.list(shoppingCart);
+        if(list != null && !list.isEmpty()){
+            //如果有，更新商品的数量即可
+            ShoppingCart cart = list.get(0);
+            cart.setNumber(cart.getNumber() + 1);
+            shoppingCartMapper.updateNumberById(cart);
+        }else{
+            //如果没有，再去添加商品到购物车
+
+            //先去判断是套餐还是菜品
+            Long dishId = shoppingCartDTO.getDishId();
+            if(dishId != null) {
+                //是菜品
+                Dish dish = dishMapper.getById(shoppingCartDTO.getDishId());
+                shoppingCart.setName(dish.getName());
+                shoppingCart.setImage(dish.getImage());
+                shoppingCart.setAmount(dish.getPrice());
+            }else{
+                //是套餐
+                Long setmealId = shoppingCartDTO.getSetmealId();
+                Setmeal setmeal = setmealMapper.getById(setmealId);
+                shoppingCart.setName(setmeal.getName());
+                shoppingCart.setImage(setmeal.getImage());
+                shoppingCart.setAmount(setmeal.getPrice());
+            }
+            shoppingCart.setNumber(1);
+            shoppingCart.setCreateTime(LocalDateTime.now());
+            shoppingCartMapper.insert(shoppingCart);
+
+        }
+
+    }
+}

2bf940ef4a09c86e44cf938e288e945c626f2851
ize250917 <GitHub>
2026-09-10 21:48:25 +0800

    优化：校验收货地址是否超出配送范围

--- sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/OrderServiceImpl.java
@@ -375,12 +375,12 @@ public class OrderServiceImpl implements OrderService {
         Integer payStatus = ordersDB.getPayStatus();
         if (payStatus == Orders.PAID) {
             //用户已支付，需要退款
-//            String refund = weChatPayUtil.refund(
-//                    ordersDB.getNumber(),
-//                    ordersDB.getNumber(),
-//                    new BigDecimal(0.01),
-//                    new BigDecimal(0.01));
-//            log.info("申请退款：{}", refund);
+            //String refund = weChatPayUtil.refund(
+            //        ordersDB.getNumber(),
+            //        ordersDB.getNumber(),
+            //        new BigDecimal(0.01),
+            //        new BigDecimal(0.01));
+            //log.info("申请退款：{}", refund);
         }
 
         // 拒单需要退款，根据订单id更新订单状态、拒单原因、取消时间
